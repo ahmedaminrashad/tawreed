54904522042900d990656313fe8e636c12b73444
mtaher_90 <mohamedtaher90mt@gmail.com>
2024-12-27 20:26:30 +0200

    DEV:-
    - share varibale on all views from AppServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,16 +24,16 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         
-        // // share all Countries with all views
-        // view()->share('countries', Country::join('country_translations', 'countries.id', 'country_translations.country_id')
-        //     ->select('countries.id as country_id', 'country_translations.name as country_name')
-        //     ->where('country_translations.locale', 'ar')
-        //     ->pluck('country_name', 'country_id')->toArray());
+        // share all Countries with all views
+        view()->share('countries', Country::join('country_translations', 'countries.id', 'country_translations.country_id')
+            ->select('countries.id as country_id', 'country_translations.name as country_name')
+            ->where('country_translations.locale', 'ar')
+            ->pluck('country_name', 'country_id')->toArray());
 
-        // // share Terms & Conditions with all views
-        // view()->share('terms_conditions', Documentation::where('key', 'terms_conditions')->first()->translate('ar')->page);
+        // share Terms & Conditions with all views
+        view()->share('terms_conditions', Documentation::where('key', 'terms_conditions')->first()->translate('ar')->page);
 
-        // // share Privacy Policy with all views
-        // view()->share('privacy_policy', Documentation::where('key', 'privacy_policy')->first()->translate('ar')->page);
+        // share Privacy Policy with all views
+        view()->share('privacy_policy', Documentation::where('key', 'privacy_policy')->first()->translate('ar')->page);
     }
 }
