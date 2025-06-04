f4384552b876c7d6b839e42f89df3ce9cff270fc
ahmedaminrashad <ahmedaminrashad@gmail.com>
2025-06-04 16:35:23 +0300

    #work category

--- resources/views/web/tenders/review.blade.php
+++ resources/views/web/tenders/review.blade.php
@@ -172,6 +172,9 @@
 
                         @if($tender->status==TenderStatus::AWARDED->value)
                             <div class="col-xs-6">
+                                <img src="{{ asset('/assets/front/img/11.svg') }}">
+                                <h5>Activity Classification</h5>
+                                <h3>Awarded</h3>
                                 <a href="#awarded_div"><i class="ri-award-fill"></i> Awarded </a>
 
                             </div>
