9a7b25c8d15dc76c28dc8d26a70402d0502ee75f
ahmed_amin <ahmedaminrashad@gmail.com>
2025-06-14 23:15:44 +0300

    Add RTL support by introducing bootstrap-rtl.css and style-rtl.css, and update head layout for language-specific styles

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -2,9 +2,18 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>@yield('title')</title>
-<link href="{{ asset('/assets/front/css/bootstrap.css') }}" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
+
+@if(app()->getLocale() == 'ar')
+<link href="{{ asset('/assets/front/css/bootstrap.css') }}" rel="stylesheet">
+<link href="{{ asset('/assets/front/css/bootstrap-rtl.css') }}" rel="stylesheet">
+<link href="{{ asset('/assets/front/css/style-rtl.css') }}" rel="stylesheet">
+@else
+<link href="{{ asset('/assets/front/css/bootstrap.css') }}" rel="stylesheet">
 <link href="{{ asset('/assets/front/css/style.css') }}" rel="stylesheet">
+@endif
+
+
 <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet">
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">
 <link rel="stylesheet" href="{{ asset('/assets/front/css/jquery.uploader.css') }}">
