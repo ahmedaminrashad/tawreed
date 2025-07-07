1a2f61e99e53e9ac22e8b9b5d55f59926908a9bc
ahmedaminrashad <ahmedaminrashad@gmail.com>
2025-07-07 17:47:37 +0300

    Add Arabic numeral conversion for contract duration input in tender creation form

--- resources/views/web/tenders/create.blade.php
+++ resources/views/web/tenders/create.blade.php
@@ -426,7 +426,23 @@
             $("#contract_end_date").val(""); // Clear the closing date if inputs are invalid
         }
     }
+    function arabicToEnglishNumbers(input) {
+    const arabic = ['٠','١','٢','٣','٤','٥','٦','٧','٨','٩'];
+    const english = ['0','1','2','3','4','5','6','7','8','9'];
+    let output = input;
+    for (let i = 0; i < arabic.length; i++) {
+        output = output.replace(new RegExp(arabic[i], 'g'), english[i]);
+    }
+    return output;
+}
 
+$("#contract_duration").on("input", function() {
+    let val = $(this).val();
+    let converted = arabicToEnglishNumbers(val);
+    if (val !== converted) {
+        $(this).val(converted);
+    }
+});
 </script>
 
 @endsection
