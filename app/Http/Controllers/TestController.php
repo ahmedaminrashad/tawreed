501b2a0a0ee0e760841bf1a73b0d50953a0acb9f
ahmedaminrashad <ahmedaminrashad@gmail.com>
2025-07-29 22:20:40 +0300

    #users control

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,6 +15,7 @@ class TestController extends Controller
     public function test()
     {
 
+        return  base_path('/');
         return Tender::query()->with('items')->find(18);
 
         $users = User::query()->get();
